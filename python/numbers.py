0b474078e4cca26d3245a68651308f6b3f31c1f3
= <=>
2024-09-27 09:54:04 +0100

    num222

---
+++ python/numbers.py
@@ -0,0 +1,10 @@
+numbers = [2,4,6,8]
+numbers_squared = []
+
+i = 0 
+while i < int(numbers):
+    numbers_squared.append.numbers[i] * numbers[i]
+    i +=1 
+
+print(numbers_squared)
+                
