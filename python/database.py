68523605e166280306159f50a927e12de37ce308
= <=>
2024-10-07 13:46:28 +0100

    aa

---
+++ python/database.py
@@ -0,0 +1,27 @@
+import mysql.connector 
+
+#config= {
+#"host": "localhost", 
+#"user": "root" , 
+#"password": "hello99"
+#"database": "test_db"
+#}
+#db_connection = mysql.connector.connect
+
+db_connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="hello99"
+)
+
+cursor=db_connection.cursor()
+create_database_query = "CREATE DATABASE test_db"
+cursor.execute(create_database_query)
+
+#commit the changes 
+db_connection.commit()
+
+#close the cursor and the connection 
+cursor.close()
+db_connection.close()
+print("Database created")
